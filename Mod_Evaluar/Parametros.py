b80d76c7a380d74e24aef399daeac8962a7dacdb
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-28 12:26:35 -0600

    Creamos modulo de parametros a evaluar

---
+++ Mod_Evaluar/Parametros.py
@@ -0,0 +1,18 @@
+class Evaluar():
+    def TEPA(self, trianguloEvaluacionPediatricaApariencia):
+        self.trianguloEvaluacionPediatricaApariencia = trianguloEvaluacionPediatricaApariencia
+    def TEPR(self, trianguloEvaluacionPediatricaRespiracion):
+        self.trianguloEvaluacionPediatricaRespiracion = trianguloEvaluacionPediatricaRespiracion
+    def TEPC(self, trianguloEvaluacionPediatricaCirculacion):
+        self.trianguloEvaluacionPediatricaCirculacion = trianguloEvaluacionPediatricaCirculacion
+    def SAT(self, sistemaAlertaTemprana):
+        self.sistemaAlertaTemprana = sistemaAlertaTemprana
+    def SAC(self, saveChild):
+        self.saveChild = saveChild
+    
+##    self.codigoAzul = 0
+##    self.codigoVerde = 0
+##    self.codigoAmarillo = 0
+##    self.codigoNaranja = 0
+##    self.codigoRojo = 0
+##    codigos= 0
