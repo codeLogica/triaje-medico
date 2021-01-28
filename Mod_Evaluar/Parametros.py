1a3e0495bad881e9b5731f3716ac750bacac8a89
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-28 17:35:24 -0600

    Modificamos modulo Parametros agregando prints para comprobar funcionalidad los metodos

--- Mod_Evaluar/Parametros.py
+++ Mod_Evaluar/Parametros.py
@@ -1,14 +1,19 @@
 class Evaluar():
     def TEPA(self, trianguloEvaluacionPediatricaApariencia):
         self.trianguloEvaluacionPediatricaApariencia = trianguloEvaluacionPediatricaApariencia
+        print(self.trianguloEvaluacionPediatricaApariencia)
     def TEPR(self, trianguloEvaluacionPediatricaRespiracion):
         self.trianguloEvaluacionPediatricaRespiracion = trianguloEvaluacionPediatricaRespiracion
+        print(self.trianguloEvaluacionPediatricaRespiracion)
     def TEPC(self, trianguloEvaluacionPediatricaCirculacion):
         self.trianguloEvaluacionPediatricaCirculacion = trianguloEvaluacionPediatricaCirculacion
+        print(self.trianguloEvaluacionPediatricaCirculacion)
     def SAT(self, sistemaAlertaTemprana):
         self.sistemaAlertaTemprana = sistemaAlertaTemprana
+        print(self.sistemaAlertaTemprana)
     def SAC(self, saveChild):
         self.saveChild = saveChild
+        print(self.saveChild)
     
 ##    self.codigoAzul = 0
 ##    self.codigoVerde = 0
