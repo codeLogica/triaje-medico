2f2f1859926f7e2b2691fcdfb10a0b069996008f
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-28 11:01:06 -0600

    Creamos modulo para cambiar frames

---
+++ change_frame.py
@@ -0,0 +1,12 @@
+class SampleApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(PaginaGeneral)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
