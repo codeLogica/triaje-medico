afa33ecd5cac36389fd4f6c2ba5a521f689f69b9
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-28 17:36:29 -0600

    Creamos el modulo del segundo parametro a evaluar: coloracion de la piel

---
+++ Mod_Evaluar/ColoracionPiel.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import app
+from Mod_Evaluar import Parametros
+
+#Se hace la llamada a el modulo de los parametros a evaluar
+paramEvaluar = Parametros.Evaluar()
+
+class Opciones():
+    def Rosada(self):
+        paramEvaluar.TEPC(True)
+        paramEvaluar.SAT(+0)
+        paramEvaluar.SAC(+0)
+    def Palida(self):
+        paramEvaluar.TEPC(False)
+        paramEvaluar.SAT(+1)
+        paramEvaluar.SAC(+0.3)
+    def Cianotica(self):
+        paramEvaluar.TEPC(False)
+        paramEvaluar.SAT(+2)
+        paramEvaluar.SAC(+1)
+    def Rubicunda(self):
+        paramEvaluar.TEPC(False)
+        paramEvaluar.SAT(+2)
+        paramEvaluar.SAC(+1)
+    def Marmorea(self):
+        paramEvaluar.TEPC(False)
+        paramEvaluar.SAT(+3)
+        paramEvaluar.SAC(+1)
+    def Purpurica(self):
+        paramEvaluar.TEPC(False)
+        paramEvaluar.SAT(+3)
+        paramEvaluar.SAC(+1)
+        
