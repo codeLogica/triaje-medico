f833b2e9eba22a8c4b25e0a77fcb4727af0307c3
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-29 13:55:35 -0600

    Creamos el modulo para evaluar signos vitales

---
+++ Mod_Evaluar/SignosVitales.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from typing import Text
+import app
+from Mod_Evaluar import Parametros
+
+#Se hace la llamada a el modulo de los parametros a evaluar
+paramEvaluar = Parametros.Evaluar()
+
+class Validar():
+    def Dato(datoIngresado):
+        return Text.isdecimal()
+        
