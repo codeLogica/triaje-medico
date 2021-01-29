50ab339f8fd4a7fed9983105a0b856499609e803
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-29 11:10:35 -0600

    Creamos el modulo con el deoceavo parametro a evaluar: la clinica del llanto

---
+++ Mod_Evaluar/Llanto.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import app
+from Mod_Evaluar import Parametros
+
+#Se hace la llamada a el modulo de los parametros a evaluar
+paramEvaluar = Parametros.Evaluar()
+
+class Opciones():
+    def Fuerte(self):
+        paramEvaluar.TEPA(True)
+        paramEvaluar.SAC(+0)
+    def Debil(self):
+        paramEvaluar.TEPA(False)
+        paramEvaluar.SAC(+0.333)
