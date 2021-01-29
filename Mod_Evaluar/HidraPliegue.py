b1fc078bec2507acfd4e301c1913c3178a3c4774
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-28 18:24:02 -0600

    Creamos el modulo del sexto parametro a evaluar: estado de hidratacion segun el signo del pliegue

---
+++ Mod_Evaluar/HidraPliegue.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import app
+from Mod_Evaluar import Parametros
+
+#Se hace la llamada a el modulo de los parametros a evaluar
+paramEvaluar = Parametros.Evaluar()
+
+class Opciones():
+    def Positivo(self):
+        paramEvaluar.SAC(+0.333)
+    def Negativo(self):
+        paramEvaluar.SAC(+0)
