eba4939782af77d196aa854f58b7e6ea00013a1e
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-29 11:04:20 -0600

    Creamos el modulo con el onceavo parametro a evaluar: el contacto visual y direccion de la mirada

---
+++ Mod_Evaluar/Visual.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import app
+from Mod_Evaluar import Parametros
+
+#Se hace la llamada a el modulo de los parametros a evaluar
+paramEvaluar = Parametros.Evaluar()
+
+class Opciones():
+    def Mantiene(self):
+        paramEvaluar.TEPA(True)
+        paramEvaluar.SAC(+0)
+    def NoMantiene(self):
+        paramEvaluar.TEPA(False)
+        paramEvaluar.SAC(+0.333)
+    def NoDirige(self):
+        paramEvaluar.TEPA(False)
+        paramEvaluar.SAC(+0.666)
