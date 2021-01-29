9ebb0ea0b70a4935bbef300440f39872f7c4b5cf
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-29 11:22:02 -0600

    Creamos el modulo con el treceavo parametro a evaluar: la capacidad del infante para ser consolado

---
+++ Mod_Evaluar/Consolabilidad.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import app
+from Mod_Evaluar import Parametros
+
+#Se hace la llamada a el modulo de los parametros a evaluar
+paramEvaluar = Parametros.Evaluar()
+
+class Opciones():
+    def Consolable(self):
+        paramEvaluar.TEPC(True)
+        paramEvaluar.SAT(+1)
+        paramEvaluar.SAC(+0)
+    def Inconsolable(self):
+        paramEvaluar.TEPC(False)
+        paramEvaluar.SAT(+2)
+        paramEvaluar.SAC(+0.666)
