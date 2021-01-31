8db21cedeca00892e8343f35d22b60078f84839c
codeLOGIC <medinarivasricardo@gmail.com>
2021-01-31 16:12:54 -0600

    Se actualizo el modulo de parametros para realizar la evaluacion de los valores

--- Mod_Evaluar/Parametros.py
+++ Mod_Evaluar/Parametros.py
@@ -1,36 +1,49 @@
+# Se reciben los valores parasados como argumentos y realiza la suma. 
+
 class Evaluar():
+    def __init__(self):
+        self.valorInicial= 0
+        self.valorTotal= 0
+        
     def TEPA(self, valorAñadido):
         self.valorAñadido = valorAñadido
         print(f"Soy el valor del Triangulo Evaluacion Pediatrica Apariencia {self.valorAñadido}")
+        return self.valorAñadido
     def TEPR(self, valorAñadido):
         self.valorAñadido = valorAñadido
         print(f"Soy el valor del Triangulo Evaluacion Pediatrica Respiracion {self.valorAñadido}")
+        return self.valorAñadido
     def TEPC(self, valorAñadido):
         self.valorAñadido = valorAñadido
         print(f"Soy el valor del Triangulo Evaluacion Pediatrica Circulacion {self.valorAñadido}")
+        return self.valorAñadido
+    
     def SAT(self, valorAñadido):
-        self.valorInicial= 0
-        self.valorTotal= 0
+        self.valorAñadido = valorAñadido
     
         if self.valorInicial == 0:
             self.valorTotal= self.valorInicial + self.valorAñadido
-            self.valorInicial = self.valorInicial + 1
+            self.valorInicial=+ 1
+            print(f"Soy el valor del Sistema Alerta Temprana {self.valorTotal}")
+            return self.valorTotal
         else:
             self.valorTotal = self.valorTotal + self.valorAñadido
+            print(f"Soy el valor del Sistema Alerta Temprana {self.valorTotal}")
+            return self.valorTotal
             
-        print(f"Soy el valor del Sistema Alerta Temprana {self.valorTotal}")
-        
     def SAC(self, valorAñadido):
-        self.valorInicial= 0.0
-        self.valorTotal= 0
+        self.valorAñadido = valorAñadido
     
         if self.valorInicial == 0:
             self.valorTotal= self.valorInicial + self.valorAñadido
-            self.valorInicial = self.valorInicial + 1
+            self.valorInicial=+ 1
+            print(f"Soy el valor de Save a Child {self.valorTotal}")
+            return self.valorTotal
         else:
             self.valorTotal = self.valorTotal + self.valorAñadido
+            print(f"Soy el valor de Save a Child {self.valorTotal}")
+            return self.valorTotal
             
-        print(f"Soy el valor de Save a Child {self.valorTotal}")
     
 ##    self.codigoAzul = 0
 ##    self.codigoVerde = 0
