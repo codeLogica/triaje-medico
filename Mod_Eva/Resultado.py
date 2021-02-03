6272616dc8a7889245afd8b5d79cf480be9168cd
codeLOGIC <medinarivasricardo@gmail.com>
2021-02-03 16:58:24 -0600

    Se modifico el modulo para hacer funcional la asignacion del codigo de color

--- Mod_Eva/Resultado.py
+++ Mod_Eva/Resultado.py
@@ -1,30 +1,41 @@
-from Mod_Eva import Parametros
-
-class CodigoColor(Parametros.Evaluar):
-    def __init___(self):
-        self.codigoAzul = 0
-        self.codigoVerde = 0
-        self.codigoAmarillo = 0
-        self.codigoNaranja = 0
-        self.codigoRojo = 0
+class CodigoColor():    
+    def __init__(self):
+        self.codigoRojo= 0
+        self.codigoNaranja= 0
+        self.codigoAmarillo= 0
+        self.codigoVerde= 0
+        self.codigoAzul= 0
     
-    def TrianguloEvaluacionPediatrica(self):
+    def TrianguloEvaluacionPediatrica(self, TEPA, TEPC, TEPR):
+        self.valorFinalTEPA= TEPA
+        self.valorFinalTEPC= TEPC
+        self.valorFinalTEPR= TEPR
+        
         if self.valorFinalTEPA== True and self.valorFinalTEPC== True and self.valorFinalTEPR== True:
             self.codigoAzul=+ 1
+            print(self.codigoAzul)
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== True and self.valorFinalTEPR== True:
             self.codigoRojo=+ 1
+            print(self.codigoRojo)
         elif self.valorFinalTEPA== True and self.valorFinalTEPC== True and self.valorFinalTEPR== False:
             self.codigoNaranja=+ 1
+            print(self.codigoNaranja)
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== True and self.valorFinalTEPR== False:
             self.codigoRojo=+ 1
+            print(self.codigoRojo)
         elif self.valorFinalTEPA== True and self.valorFinalTEPC== False and self.valorFinalTEPR== True:
             self.codigoNaranja=+ 1
+            print(self.codigoNaranja)
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== False and self.valorFinalTEPR== True:
             self.codigoRojo=+ 1
+            print(self.codigoRojo)
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== False and self.valorFinalTEPR== False:
             self.codigoRojo=+ 1
+            print(self.codigoRojo)
     
-    def SistemaAlertaTemprana(self):
+    def SistemaAlertaTemprana(self, valorFinalSAT):
+        self.valorFinalSAT= valorFinalSAT
+        
         if self.valorFinalSAT==0:
             self.codigoAzul=+ 1
         elif self.valorFinalSAT==1:
@@ -35,8 +46,10 @@ class CodigoColor(Parametros.Evaluar):
             self.codigoNaranja=+ 1
         elif self.valorFinalSAT>3:
             self.codigoRojo=+ 1
-    
-    def SaveChild(self):
+
+    def SaveChild(self, valorFinalSAC):
+        self.valorFinalSAC= valorFinalSAC
+        
         if self.valorFinalSAC==0 and self.valorFinalSAC<1:
             self.codigoAzul=+1
         elif self.valorFinalSAC==1 and self.valorFinalSAC<2:
@@ -47,5 +60,5 @@ class CodigoColor(Parametros.Evaluar):
             self.codigoNaranja=+1
         elif self.valorFinalSAC >=4:
             self.codigoRojo=+1  
-
+    
         
