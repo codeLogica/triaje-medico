c45f73146c33b7dc670bb5d08bcc848fcfdd502d
codeLOGIC <medinarivasricardo@gmail.com>
2021-02-08 17:44:04 -0600

    Se modifico modulo resultado para validar datos de TEP y dar u a .git Imagenes Mod_Eva README.md app.py bebe.gif

--- Mod_Eva/Resultado.py
+++ Mod_Eva/Resultado.py
@@ -1,3 +1,6 @@
+#En esta modulo se reciben los resultados de analizar los valores clinicos. Se analiza los nuevos valores y se arroja un resultado de codigo de color segun la gravedad...  
+#Tenemos un inicializador con los parametros a checar, junto con las variables del Triangulo de Evaluacion Pediatrica. Un metodo que verifica que se tengan todos los datos completos y finalmente uno que arroje un resultado final. 
+
 class CodigoColor():    
     def __init__(self):
         self.codigoRojo= 0
@@ -5,48 +8,57 @@ class CodigoColor():
         self.codigoAmarillo= 0
         self.codigoVerde= 0
         self.codigoAzul= 0
+        
+        self.valorFinalTEPA= None
+        self.valorFinalTEPC= None
+        self.valorFinalTEPR= None
     
     def TrianguloEvaluacionPediatricaApariencia(self, TEPA):
         self.valorFinalTEPA= TEPA
-        self.validarValores()
+        self.ValidarValoresTriangulo()
         return self.valorFinalTEPA
     def TrianguloEvaluacionPediatricaCirculacion(self, TEPC):
         self.valorFinalTEPC= TEPC
-        self.validarValores()
+        self.ValidarValoresTriangulo()
         return self.valorFinalTEPC
     def TrianguloEvaluacionPediatricaRespiracion(self, TEPR):
         self.valorFinalTEPR= TEPR
-        self.validarValores()
+        self.ValidarValoresTriangulo()
         return self.valorFinalTEPR
     
-    def validarValores(self):
+    def ValidarValoresTriangulo(self):
         if self.valorFinalTEPA != None and self.valorFinalTEPC != None and self.valorFinalTEPR != None:
             self.TrianguloEvaluacionPediatrica()
-        else:
-            pass
-            
+
     def TrianguloEvaluacionPediatrica(self):
         if self.valorFinalTEPA== True and self.valorFinalTEPC== True and self.valorFinalTEPR== True:
             self.codigoAzul=+ 1
             print(f"Soy el valor del codigo AZUL TEP:{self.codigoAzul}")
+            return self.codigoAzul
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== True and self.valorFinalTEPR== True:
             self.codigoRojo=+ 1
             print(f"Soy el valor del codigo ROJO TEP:{self.codigoRojo}")
+            return self.codigoRojo
         elif self.valorFinalTEPA== True and self.valorFinalTEPC== True and self.valorFinalTEPR== False:
             self.codigoNaranja=+ 1
             print(f"Soy el valor del codigo NARANJA TEP:{self.codigoNaranja}")
+            return self.codigoNaranja
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== True and self.valorFinalTEPR== False:
             self.codigoRojo=+ 1
             print(f"Soy el valor del codigo ROJO TEP:{self.codigoRojo}")
+            return self.codigoRojo
         elif self.valorFinalTEPA== True and self.valorFinalTEPC== False and self.valorFinalTEPR== True:
             self.codigoNaranja=+ 1
             print(f"Soy el valor del codigo NARANJA TEP:{self.codigoNaranja}")
+            return self.codigoNaranja
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== False and self.valorFinalTEPR== True:
             self.codigoRojo=+ 1
             print(f"Soy el valor del codigo ROJO TEP:{self.codigoRojo}")
+            return self.codigoRojo
         elif self.valorFinalTEPA== False and self.valorFinalTEPC== False and self.valorFinalTEPR== False:
             self.codigoRojo=+ 1
             print(f"Soy el valor del codigo ROJO TEP:{self.codigoRojo}")
+            return self.codigoRojo
     
     def SistemaAlertaTemprana(self, valorFinalSAT):
         self.valorFinalSAT= valorFinalSAT
