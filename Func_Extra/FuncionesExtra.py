9d3abd99fda511e75c6a2fd6466dfdf37253c8d8
codeLOGIC <medinarivasricardo@gmail.com>
2021-03-03 12:34:05 -0600

    Modificaciones menores. Eliminar caracteres romanos como ´ñ´

--- Func_Extra/FuncionesExtra.py
+++ Func_Extra/FuncionesExtra.py
@@ -11,12 +11,11 @@ def ValidarMayuscula(*letras):
         else:
             return False
         
-    #DevolverNombre(validacion)
+    DevolverNombre(validacion)
     return all(validacion)
 
 def DevolverNombre(nombre):
-    nNombre= f"Paciente {nombre}"
-    return nNombre
+    nNombre= nombre
 
 #Funcion para corrobar el ingreso de la fecha de nacimiento con datos numericos y con 8 digitos a lo maximo (2 dia/2 mes/4 año).
 def ValidarEdad(*f_nacimiento):
@@ -31,7 +30,7 @@ def ValidarEdad(*f_nacimiento):
             
             listaDias= ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
             listaMeses= ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-            listaAños= list(range(1901, 3001))
+            listaAgnos= list(range(1901, 3001))
             
             #Corroborar que la fecha sea valida. 
             if len(validacion[0])==2:
@@ -41,7 +40,7 @@ def ValidarEdad(*f_nacimiento):
                 if (validacion[0][2:4]) not in listaMeses:
                     return False
             elif len(validacion[0])==8:
-                if int(validacion[0][4:]) not in listaAños:
+                if int(validacion[0][4:]) not in listaAgnos:
                     return False
                 elif len(validacion[0])==8:
                     hoy= datetime.now()
@@ -68,25 +67,25 @@ def CalcularEdad(f_nacimiento_comprobacion):
         diaNacimiento= datetime.strptime(f_nacimiento_comprobacion, "%d%m%Y")
         calculoEdad= (diaActual - diaNacimiento)
                             
-        diferenciaAños= calculoEdad.days / 365
+        diferenciaAgnos= calculoEdad.days / 365
         diferenciaMeses= calculoEdad.days / 30  
         diferenciaDias= calculoEdad.days
         
-        DevolverEdad(diferenciaAños, diferenciaMeses, diferenciaDias)
+        DevolverEdad(diferenciaAgnos, diferenciaMeses, diferenciaDias)
 
-def DevolverEdad(años, meses, dias):  
+def DevolverEdad(agnos, meses, dias):  
     edadPx= None                          
     if dias < 28:
-        edadPx= f"Recien Nacido {dias} dias"
+        edadPx= f"{dias} dias"
     elif meses < 12:
-        edadPx= f"Lactante Menor {meses} meses"
+        edadPx= f"{meses} meses"
     elif meses < 24:
-        edadPx= f"Lactante Mayor {meses} meses"
-    elif años >2 and años <6:
-        edadPx= f"Preescolar {años} años"
-    elif años >6 and años <10:
-        edadPx= f"Escolar {años} años"
-    elif años > 11 and años <19:
-        edadPx= f"Adolescente {años} años"
+        edadPx= f"{meses} meses"
+    elif agnos >2 and agnos <6:
+        edadPx= f"{agnos} años"
+    elif agnos >6 and agnos <10:
+        edadPx= f"{agnos} años"
+    elif agnos > 11 and agnos <19:
+        edadPx= f"{agnos} años"
     
     return edadPx
